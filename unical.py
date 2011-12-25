95e569693c10ee7594b19ae9392547da5cc6fb7a
Jishnu <jishnu7@gmail.com>
2011-12-25 13:02:44 +0530

    working

--- unical.py
+++ unical.py
@@ -21,10 +21,9 @@
 import urllib
 from BeautifulSoup import BeautifulSoup
 import re
-import cPickle as Pickle
+import cPickle
 import os
-#import MySQLdb
-import twitterbot
+from twitterbot import Twitter
 
 # File to store site data
 PICKLE_FILE = "lastupdate.p"
@@ -33,61 +32,53 @@ PICKLE_FILE = "lastupdate.p"
 # More than one needed to avoid problems if one entry is deleted
 ENTRIES = 2
 
-#DBHOST = "localhost"
-#DBPASS = "root"
-#DBUSER = "root"
-#DB = "test"
-
-#db = MySQLdb.connect(host=DBHOST, user=DBUSER, passwd=DBPASS, db=DB)
-
-#def send_sms(msg,number):
-#    print msg number
-
-#def send_mail(msg, email):
-#    print msg. email
-
-#def push(text, content_type, link):
-#    cursor = db.cursor()
-#    query = "INSERT INTO post (content, type, link) VALUES (\"%s\", %s, \"%s\")" % (text, content_type, link)
-#    cursor.execute(query)
-#    print "here"
-#    rows = int(cursor.rowcount)
-#    print rows
-
-def pull(query):
-    cursor = db.cursor()
-    cursor.execute(query)
-    return int(cursor.rowcount)
-
-def pickling(data):
-    pfile = open(PICKLE_FILE, 'wb')
-    Pickle.dump(data, pfile)
-    pfile.close()
-    return
-
-def unpickling():
-    data = []
-    if os.path.isfile(PICKLE_FILE):
-        pfile = open(PICKLE_FILE, 'rb')
-        data = Pickle.load(pfile)
+class Pickle():
+    def pickling(self,data):
+        pfile = open(PICKLE_FILE, 'wb')
+        cPickle.dump(data, pfile)
         pfile.close()
-        return data
-    else:
-        return None
+        return
 
-def update_search(link):
-    lastupdate = unpickling()
-    if lastupdate:
-        for i in range(0, ENTRIES):
-            if lastupdate[i]['link'] == link:
-                # No updates
-                return True
-        # Update found
-    return False
+    def unpickling(self):
+        data = []
+        if os.path.isfile(PICKLE_FILE):
+            pfile = open(PICKLE_FILE, 'rb')
+            data = cPickle.load(pfile)
+            pfile.close()
+            return data
+        else:
+            return None
+
+    def update_search(self,link):
+        lastupdate = self.unpickling()
+        if lastupdate:
+            for i in range(0, ENTRIES):
+                if lastupdate[i]['link'] == link:
+                    # No updates
+                    return True
+            # Update found
+        return False
+
+'''
+def search(datas):
+    terms = {
+                'b.tech' : ['b.tech', 'btech', 'b tech', 'bachelor of technology'],
+                'b.arch' : ['b.arch', 'barch', 'b arch', 'bachelor of architecture'],
+                'm.tech' : ['m.tech', 'mtech', 'm tech']
+            }
+
+    for term in terms:
+        temp = list()
+        for subterm in term:
+            for data in datas:
+                if data['text'].lower().find(subterm) !=-1:
+                    temp.append(data['text'])
+        if len(temp) != 0:
+            print "found"
+'''
 
 def fetch_data():
     web = urllib.urlopen("http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=744&pop=1&page=0&Itemid=324")
-    #web = urllib.urlopen("notification.htm")
     data = BeautifulSoup(web)
     i = 10
     top = 0
@@ -95,14 +86,16 @@ def fetch_data():
     update = []
     new_data = []
 
+    pck = Pickle()
+
     while 1:
-        print "-------------------------------------"
+        #print "-------------------------------------"
         try:
             row = data.findAll("tr")[i].findAll("td")
             i = i+2
         except:
             # reached end
-            pickling(update)
+            pck.pickling(update)
             break
         for column in row:
             try:
@@ -121,23 +114,23 @@ def fetch_data():
             # trim
             text = text.strip()
 
-            if update_search(link):
+            if pck.update_search(link):
             # Search data found
             # No new update
                 j = 0
                 while len(update) < ENTRIES:
-                    lastupdate = unpickling()
+                    lastupdate = pck.unpickling()
                     update.append(lastupdate[j])
                     j = j + 1
                 if(j==2):
                     print "No new updates"
-                pickling(update)
+                pck.pickling(update)
                 return new_data
             else:
             # Search data not found
             # Update Found
-                print link
-                print text
+                #print link
+                #print text
                 temp = {'text' : text, 'link' : link}
                 new_data.append(temp)
                 if top < ENTRIES:
@@ -145,25 +138,26 @@ def fetch_data():
                     top = top + 1
     return new_data
 
-def search(datas):
-    terms = {
-                'b.tech' : ['b.tech', 'btech', 'b tech', 'bachelor of technology'],
-                'b.arch' : ['b.arch', 'barch', 'b arch', 'bachelor of architecture'],
-                'm.tech' : ['m.tech', 'mtech', 'm tech']
-            }
+def truncate(s):
+    ''' Function to trucate long string in a nicer way '''
+    charmax = 120
+    suffix ="..."
+    length = len(s)
 
-    for term in terms:
-        temp = list()
-        for subterm in term:
-            for data in datas:
-                if data['text'].lower().find(subterm) !=-1:
-                    temp.append(data['text'])
-        if len(temp) != 0:
-            print "found"
+    if(length <= charmax):
+        return s
+    else:
+        # return string to nearest 'space'
+        end = s.rfind(' ',0,charmax-3)
+        return s[0:end] + suffix
 
-#        send_mail("")
-#        send_sms("")
+updates = fetch_data()
+updates.reverse()
 
-a = fetch_data()
-if a != None:
-    search(a)
+instance=Twitter()
+
+for item in updates:
+    try:
+        instance.tweet(truncate(item['text'])+" http://universityofcalicut.info"+item['link'])
+    except:
+        continue
