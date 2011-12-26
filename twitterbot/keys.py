daa9db11b5de2ddcc9757336ed118505441f74db
Jishnu <jishnu7@gmail.com>
2011-12-26 13:58:28 +0530

    new key file

---
+++ twitterbot/keys.py
@@ -0,0 +1,6 @@
+# Add your keys here. Use access key generation script to get access permissions on account.
+CONSUMER_KEY    = ' '
+CONSUMER_SECRET = ' '
+
+ACCESS_KEY      = ' '
+ACCESS_SECRET   = ' '
