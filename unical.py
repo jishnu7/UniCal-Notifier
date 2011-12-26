e5d8e7322bf2ebb8aed844f0f81b0fa1e35d55b5
Jishnu <jishnu7@gmail.com>
2011-12-26 17:20:02 +0530

    Converted everything to oop

--- unical.py
+++ unical.py
@@ -29,23 +29,27 @@ from twitterbot import Twitter
 # More than one needed to avoid problems if one entry is deleted
 ENTRIES = 2
 
+# Urls to the updates listing pages
 URL = {
         'Notifications' : 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=744&pop=1&page=0&Itemid=324',
-        #'Timetable' : 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=745&pop=1&page=0&Itemid=325'#,
+        'Timetable' : 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=745&pop=1&page=0&Itemid=325',
         'Results' : 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=792&pop=1&page=0&Itemid=342'
        }
 
 class Pickle():
+    ''' Class to manage pickling operations '''
     def __init__(self, filename):
         self.filename = filename
 
     def pickling(self,data):
+        ''' Save to pickle file '''
         pfile = open(self.filename, 'wb')
         cPickle.dump(data, pfile)
         pfile.close()
         return
 
     def unpickling(self):
+        ''' Get values from pickle file '''
         data = []
         if os.path.isfile(self.filename):
             pfile = open(self.filename, 'rb')
@@ -56,6 +60,7 @@ class Pickle():
             return None
 
     def update_search(self,link):
+        ''' Compare date with data on pickle file.'''
         lastupdate = self.unpickling()
         if lastupdate:
             for i in range(0, ENTRIES):
@@ -65,119 +70,139 @@ class Pickle():
             # Update found
         return False
 
-'''
-def search(datas):
-    terms = {
-                'b.tech' : ['b.tech', 'btech', 'b tech', 'bachelor of technology'],
-                'b.arch' : ['b.arch', 'barch', 'b arch', 'bachelor of architecture'],
-                'm.tech' : ['m.tech', 'mtech', 'm tech']
-            }
+class unical():
+    def check_update(self):
+        updates = []
+        for page in URL:
+            temp = self.fetch_data(page, URL[page])
+            temp.reverse()
+            updates.extend(temp)
+        return updates
 
-    for term in terms:
-        temp = list()
-        for subterm in term:
-            for data in datas:
-                if data['text'].lower().find(subterm) !=-1:
-                    temp.append(data['text'])
-        if len(temp) != 0:
-            print "found"
-'''
+    def fetch_data(self, page, url):
+        ''' Fetch data from passed link '''
+        web = urllib.urlopen(url)
+        data = BeautifulSoup(web)
+        i = 10
+        top = 0
 
-def fetch_data(page, url):
-    web = urllib.urlopen(url)
-    data = BeautifulSoup(web)
-    i = 10
-    top = 0
+        update = []
+        new_data = []
 
-    update = []
-    new_data = []
+        # File to store site data
+        pickle_file = "lastupdate_"+str(page)+".p"
+        pck = Pickle(pickle_file)
 
-    # File to store site data
-    pickle_file = "lastupdate_"+str(page)+".p"
-    pck = Pickle(pickle_file)
-
-    while 1:
-        #print "-------------------------------------"
-        try:
-            row = data.findAll("tr")[i].findAll("td")
-            i = i+2
-        except:
-            # reached end
-            pck.pickling(update)
-            break
-        for column in row:
+        while 1:
+            #print "-------------------------------------"
             try:
-                link = column.findAll("a")[0]['href']
+                row = data.findAll("tr")[i].findAll("td")
+                i = i+2
+            except:
+                # reached end
+                pck.pickling(update)
+                break
+            j = 0
+            for column in row:
+                j += 1
+                # First colum in the list is a gif image, which some times may have links too. We need to skip this
+                if j == 1:
+                    continue
+
+                try:
+                    link = column.findAll("a")[0]['href']
+                except:
+                    continue
+                text = column.renderContents()
+
+                # Remove comments
+                text = re.sub(r"<!--(.*?)-->","", text)
+                # Remove HTML tags
+                text = re.sub(r"<[^>]+>","", text)
+                # Replace html characters
+                text = re.sub(r"&nbsp;","", text)
+                text = re.sub(r"&amp;","&",text)
+                # trim
+                text = text.strip()
+
+                if pck.update_search(link):
+                # Search data found
+                # No new update
+                    j = 0
+                    while len(update) < ENTRIES:
+                        lastupdate = pck.unpickling()
+                        update.append(lastupdate[j])
+                        j = j + 1
+                    if(j==2):
+                        print "No new updates on "+page
+                    pck.pickling(update)
+                    return new_data
+                else:
+                # Search data not found
+                # Update Found
+                    #print link
+                    #print text
+                    temp = {'text' : page+' : '+text, 'link' : link}
+                    new_data.append(temp)
+                    if top < ENTRIES:
+                        update.append(temp)
+                        top = top + 1
+        return new_data
+
+class text():
+    def search(self, datas, terms):
+        ''' 
+            Search for a purticular term 
+            'terms' should be a dictionary of array of values, or an array of array of values
+            eg:
+            terms = {
+                        'b.tech' : ['b.tech', 'btech', 'b tech', 'bachelor of technology'],
+                        'b.arch' : ['b.arch', 'barch', 'b arch', 'bachelor of architecture']
+                    }
+            'datas' is the data ferched by the fetch_data function. Which is also an array of array values.
+        '''
+        for term in terms:
+            temp = list()
+            for subterm in term:
+                for data in datas:
+                    if data['text'].lower().find(subterm) !=-1:
+                        temp.append(data['text'])
+                        # Remove it from data for avoiding duplicates
+                        datas.remove(data)
+            if len(temp) != 0:
+                return temp
+            return None
+
+    def truncate(self, s):
+        ''' Function to trucate long string in a nicer way '''
+        charmax = 120
+        suffix ="..."
+        length = len(s)
+
+        if(length <= charmax):
+            return s
+        else:
+            # return string to nearest 'space'
+            end = s.rfind(' ',0,charmax-3)
+            return s[0:end] + suffix
+
+if __name__ == "__main__":
+    unical = unical()
+    updates = unical.check_update()
+    instance = Twitter()
+    txt = text()
+
+    for item in updates:
+        if item:
+            if str(item['link']).startswith('/'):
+                link = "http://universityofcalicut.info"+item['link']
+            elif str(item['link']).startswith('http://'):
+                link = item['link']
+            else:
+                link = "http://universityofcalicut.info/"+item['link']
+
+            try:
+                instance.tweet(txt.truncate(item['text'])+' '+ link)
+                #print txt.truncate(item['text'])+' '+ link
             except:
                 continue
-            text = column.renderContents()
-
-            # Remove comments
-            text = re.sub(r"<!--(.*?)-->","", text)
-            # Remove HTML tags
-            text = re.sub(r"<[^>]+>","", text)
-            # Replace html characters
-            text = re.sub(r"&nbsp;","", text)
-            text = re.sub(r"&amp;","&",text)
-            # trim
-            text = text.strip()
-
-            if pck.update_search(link):
-            # Search data found
-            # No new update
-                j = 0
-                while len(update) < ENTRIES:
-                    lastupdate = pck.unpickling()
-                    update.append(lastupdate[j])
-                    j = j + 1
-                if(j==2):
-                    print "No new updates on "+page
-                pck.pickling(update)
-                return new_data
-            else:
-            # Search data not found
-            # Update Found
-                #print link
-                #print text
-                temp = {'text' : page+' : '+text, 'link' : link}
-                new_data.append(temp)
-                if top < ENTRIES:
-                    update.append(temp)
-                    top = top + 1
-    return new_data
-
-def truncate(s):
-    ''' Function to trucate long string in a nicer way '''
-    charmax = 120
-    suffix ="..."
-    length = len(s)
-
-    if(length <= charmax):
-        return s
-    else:
-        # return string to nearest 'space'
-        end = s.rfind(' ',0,charmax-3)
-        return s[0:end] + suffix
-
-updates = []
-for page in URL:
-    temp = fetch_data(page, URL[page])
-    temp.reverse()
-    updates.extend(temp)
-
-instance=Twitter()
-
-for item in updates:
-    if item:
-        if str(item['link']).startswith('/'):
-            link = "http://universityofcalicut.info"+item['link']
-        elif str(item['link']).startswith('http://'):
-            link = item['link']
-        else:
-            link = "http://universityofcalicut.info/"+item['link']
-
-        try:
-            instance.tweet(truncate(item['text'])+' '+ link)
-            #print truncate(item['text'])+' '+ link
-        except:
-            continue
